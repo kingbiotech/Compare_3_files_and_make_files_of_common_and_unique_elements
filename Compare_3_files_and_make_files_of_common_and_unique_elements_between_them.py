8cf2aef53fd5fe95d20e5a3d383595e734ca42b2
kingbiotech <63943833+kingbiotech@users.noreply.github.com>
2022-04-27 18:58:41 +0530

    Add files via upload

---
+++ Compare_3_files_and_make_files_of_common_and_unique_elements_between_them.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python3
+__author__ = "Dr. Kamlesh K. Sahu"
+__version__ = "v 0.1"
+
+
+os1a = input("Enter first file  - ")
+os1  = open(os1a)
+os2a = input("Enter second file - ")
+os2  = open(os2a)
+os3a = input("Enter third file  - ")
+os3  = open(os3a)
+
+cm12  = open("Common_in_1_2.txt", "w")
+cm23  = open("Common_in_2_3.txt", "w")
+cm13  = open("Common_in_1_3.txt", "w")
+cm123 = open("Common_in_1_2_3.txt", "w")
+
+unique1  = open("Unique_in_"+os1a, "w")
+unique2  = open("Unique_in_"+os2a, "w")
+unique3  = open("Unique_in_"+os3a, "w")
+
+l1 = []
+l2 = []
+l3 = []
+common12 = []
+common23 = []
+common13 = []
+common123 = []
+
+total12 = []
+total23 = []
+total13 = []
+
+u1 = []
+u2 = []
+u3 = []
+
+
+for line in os1:
+	l1.append(line.strip())
+	
+for line in os2:
+	l2.append(line.strip())
+	
+for line in os3:
+	l3.append(line.strip())
+
+#print("Common lines =======================================")
+for i in l1:
+	if i in l2:
+		common12.append(i)
+		cm12.write(i)
+		cm12.write("\n")
+
+for i in l2:
+	if i in l3:
+		common23.append(i)
+		cm23.write(i)
+		cm23.write("\n")
+
+for i in l1:
+	if i in l3:
+		common13.append(i)
+		cm13.write(i)
+		cm13.write("\n")
+
+for i in common12:
+	if i in l3:
+		common123.append(i)
+		cm123.write(i)
+		cm123.write("\n")
+#print("Common lines Done===================================")
+
+#print("Unique lines =======================================")
+
+total12 = l1+l2
+total23 = l2+l3
+total13 = l1+l3
+
+for i in l1:
+	if i not in total23:
+		u1.append(i)
+		unique1.write(i)
+		unique1.write("\n")
+
+for i in l2:
+	if i not in total13:
+		u2.append(i)
+		unique2.write(i)
+		unique2.write("\n")
+
+for i in l3:
+	if i not in total12:
+		u3.append(i)
+		unique3.write(i)
+		unique3.write("\n")
+
+#print("Unique lines Done===================================")
+
+
+print("----------------------------------------------------------------------------")
+print("Common in \""+ os1a +"\" and \""+ os2a + "\" are ->", len(common12))
+print("Common in \""+ os2a +"\" and \""+ os3a + "\" are ->", len(common23))
+print("Common in \""+ os1a +"\" and \""+ os3a + "\" are ->", len(common13))
+print("Common in \""+ os1a +"\" and \""+ os2a + "\" and \""+ os3a + "\" are ->", len(common123))
+print("----------------------------------------------------------------------------")
+print("Unique in \""+ os1a +"\" but not present in \""+ os2a + "\" and \""+ os3a +"\"are ->", len(u1))
+print("Unique in \""+ os2a +"\" but not present in \""+ os1a + "\" and \""+ os3a +"\"are ->", len(u2))
+print("Unique in \""+ os3a +"\" but not present in \""+ os1a + "\" and \""+ os2a +"\"are ->", len(u3))
+print("----------------------------------------------------------------------------")
+print("check output files in the same folder\nDone")
+
+
+os1.close()
+os2.close()
+os2.close()
+cm12.close()
+cm23.close()
+cm13.close()
+cm123.close()
+unique1.close()
+unique2.close()
+unique3.close()
